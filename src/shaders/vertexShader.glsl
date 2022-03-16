26e63567eeb4d7feaabf5d19b232f50a5a728c93
3ddimensional <none@mail.com>
2022-03-16 12:49:04 +0200

    Matrices and Uniform Variables

--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -5,8 +5,10 @@ in vec2 textureCoords;
 
 out vec2 pass_textureCoords;
 
+uniform mat4 transormationMatrix;
+
 void main(void) {
 
-    gl_Position = vec4(position.xyz, 1.0);
+    gl_Position = transormationMatrix * vec4(position.xyz, 1.0);
     pass_textureCoords = textureCoords;
 }
