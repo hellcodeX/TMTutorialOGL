5c46d942e25a7e8c30d1c321114b7ab2da92b4e2
3ddimensional <none@mail.com>
2022-03-18 10:47:34 +0200

    Specular Lighting complete!

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -108,13 +108,13 @@ public class MainGameLoop {
 //        ModelTexture texture = new ModelTexture(loader.loadTexture("brick-wall"));
 
         RawModel model = OBJLoader.loadObjModel("dragon", loader);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("blue"));
+        ModelTexture texture = new ModelTexture(loader.loadTexture("yellow"));
         texture.setShineDumper(10);
         texture.setReflectivity(1);
         TexturedModel staticModel = new TexturedModel(model, texture);
 
         Entity entity = new Entity(staticModel, new Vector3f(0, -3, -25), 0, 0, 0, 1);
-        Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
+        Light light = new Light(new Vector3f(200, 200, 100), new Vector3f(1, 1, 1));
 
 
         Camera camera = new Camera();
